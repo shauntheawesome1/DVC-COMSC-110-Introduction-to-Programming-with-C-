2cb82a37f186f08d106bc2534ea9f83768a95e8b
shauntheawesome1 <Shaun.munshi@gmail.com>
2018-10-07 14:08:14 -0700

    Create myCity1.cpp

---
+++ myCity1.cpp
@@ -0,0 +1,33 @@
+/*
+Shaun Munshi
+2/25/18
+Assignment 3.3: myCity1.cpp
+COMSC-110 Intro to Programming
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int wedWeather = 57;
+	int thursWeather = 52;
+	int friWeather = 52;
+	int satWeather = 53;
+	int sunWeather = 55;
+	
+	cout << " " << endl;
+	
+	cout << "San Jose, California forecast high temperatures: " << endl;
+	
+	cout << "Wednesday, Feb 28, " << wedWeather << " degrees" << endl;
+	cout << "Thursday, March 1, " << thursWeather << " degrees" << endl;
+	cout << "Friday, March 2, " << friWeather << " degrees" << endl;
+	cout << "Saturday, March 3, " << satWeather << " degrees" << endl;
+	cout << "Sunday, March 4, " << sunWeather << " degrees" << endl;
+	
+	cout << " " << endl;
+	
+	cout << "Source: Weather.com" << endl;
+}
