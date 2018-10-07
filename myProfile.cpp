f46d866760ce85ee26a95319bc4c7ec2f949959e
shauntheawesome1 <Shaun.munshi@gmail.com>
2018-10-07 14:09:29 -0700

    Create myProfile.cpp

---
+++ myProfile.cpp
@@ -0,0 +1,24 @@
+/*
+Shaun Munshi
+2/25/18
+Assignment 3.2: myProfile.cpp
+COMSC-110 Intro to Programming
+*/
+
+#include <iostream>
+#include <string> 
+using namespace std; 
+  
+int main() 
+{ 
+  int age = 32; //in years 
+  double birthWeight = 5.63; // in lbs
+  string firstName = "Shaun"; 
+  char gender = 'M'; // Assuming 2 genders
+
+
+  cout << "My age is " << age << endl;
+  cout << "My birth weight was " << birthWeight << " pounds" << endl;
+  cout << "My first name is " << firstName << endl;
+  cout << "My gender is " << gender << endl;
+} 
